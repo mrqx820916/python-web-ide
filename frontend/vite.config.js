21ddb8cbbdabf76cc8004bc82b7b5d51f4b087e1
mrqx <mrqx820916@gmail.com>
2024-11-09 16:37:25 +0800

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/python-web-ide/' : '/',
+  base: '/python-web-ide/',
   plugins: [vue()],
   resolve: {
     alias: {
